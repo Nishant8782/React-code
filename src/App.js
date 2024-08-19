338732076de0aa27f3b85d7f0ef51a385aa8f098
Nishant8782 <nishantsharma8782@gmail.com>
2024-08-19 12:50:52 +0530

    my first react app

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name="Nishant sharma";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{name}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React<br/><br/> with {name}
         </a>
       </header>
     </div>
